9206a6643b97d03e18da28568703d9ababd4b3dd
석진일 <>
2020-02-01 02:48:16 +0900

    Doc : Update 1. 엑셀파일 시트이름 가져오기(19년 1학기 시트만) 2. 시트별로 데이터 프레임 만들기 3. 데이터에 날짜 기록하기

--- 자동화_과학실사용일지_200131/자동화_과학실사용일지 만들기.py
+++ 자동화_과학실사용일지_200131/자동화_과학실사용일지 만들기.py
@@ -1,20 +1,58 @@
 # import win32com.client as win32
 # import win32gui
 import pandas as pd
+import openpyxl
 
 # 1. basepath 설정
 PATH_HWP = '/Users/mac/Documents/python_hwp/자동화_과학실사용일지_200131/timetable.hwp'
 PATH_EXCEL = '/Users/mac/Documents/python_hwp/자동화_과학실사용일지_200131/science.xlsx'
+PATH_EXCEL_RESULT = '/Users/mac/Documents/python_hwp/자동화_과학실사용일지_200131/science_sel.xlsx'
 # 2. 한/글 object 생성
 # 3. 엑셀파일 불러오기
-xl = pd.ExcelFile(PATH_EXCEL)
-sht_names = xl.sheet_names
-sht_names_new = []
-for name in sht_names:
-    if int(name.split('.')[0]) < 19 and int(name.split('.')[1]) < 9:
-        sht_names_new.append(name)
+def sel_sht_name(path, year, month):
+    xl = pd.ExcelFile(path)
+    sht_names = xl.sheet_names
+    sht_names_new = []
+    for name in sht_names:
+        if int(name.split('.')[0]) > year and int(name.split('.')[1]) < month:
+            sht_names_new.append(name)
+    return sht_names_new
 
-print(sht_names_new)
 
-# 4. 
+sht_name_new = sel_sht_name(PATH_EXCEL, 18, 9)  # 19년 9월 이하 시트 이름만 수집
+
+df_res = pd.DataFrame()
+
+for i in range(23):
+    df_xl = pd.DataFrame()
+    sht_name_new[i] = sht_name_new[i].replace('-','~')
+    df_xl = pd.read_excel(PATH_EXCEL, sheet_name=sht_name_new[i], usecols={0, 1, 2, 5}) 
+
+    def clean_row(row):
+        return str(row).replace('\n', '')
+
+    def write_day(row):
+        day = sht_name_new[i].split('~')[0].split('.')
+        if row == '월':
+            return f'{int(day[1])}.{int(day[2])}'
+        elif row == '화':
+            return f'{int(day[1])}.{int(day[2]) + 1}'
+        elif row == '수':
+            return f'{int(day[1])}.{int(day[2]) + 2}'
+        elif row == '목':
+            return f'{int(day[1])}.{int(day[2]) + 3}'
+        elif row == '금':
+            return f'{int(day[1])}.{int(day[2]) + 4}'
+
+
+    df_xl['요일'] = df_xl['요일'].apply(clean_row) # 요일에 '\n'을 공백 처리
+
+    df_xl['수업일'] = df_xl['요일'].apply(write_day) # 시트이름에서 요일을 추출 하여 월, 화, 수, 목, 금에 맞게 넣기
+    df_res = pd.concat([df_res, df_xl])
+
+
+# 월요일 = 
+# df_xl.to_excel(PATH_EXCEL_RESULT, sheet_name=sht_name_new[0])
+
+
 
